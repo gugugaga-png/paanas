6375064110be1e9dc00cb58bb960a1b3c4a5f1f0
putry68 <putryherliana55@gmail.com>
2025-06-10 13:47:27 +0700

    landing  page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,19 @@
         <div class="col-7">
             <div style="width: 60rem">
                 <p class="fw-bold" style="font-size: 50px; width: ;">Expand your spendings in a proper way</p>
+                <p style="font-size :20px; width:50rem;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo provident quod animi tempora debitis cumque, dolore accusamus delectus doloribus odit illum praesentium exercitationem voluptate aspernatur nemo ipsa deserunt consectetur eum.</p>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-7">
+            <div style="width: 60rem">
+                <p class="fw-bold" style="font-size: 50px; width: ;">Get your annual summary of your money</p>
+                <p style="font-size :20px; width:50rem;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo provident quod animi tempora debitis cumque, dolore accusamus delectus doloribus odit illum praesentium exercitationem voluptate aspernatur nemo ipsa deserunt consectetur eum.</p>
+            </div>
+        </div>
+        <div class="col-5">
+            <img src="{{ asset('images/statchart.svg') }}" alt="Banner" class="px-7">
         </div>
     </div>
 </div>
